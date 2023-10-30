c2c46a49739c5f6a2eabd2e9d1400ff75db4a55e
saberhark <saberhark6@gmail.com>
2023-10-30 17:09:41 +0900

    조이스틱

--- 프로그래머스/고득점 키트/06.탐욕법/조이스틱.py
+++ 프로그래머스/고득점 키트/06.탐욕법/조이스틱.py
@@ -27,47 +27,71 @@ def solution(name):
 
 def solution1(name):
     answer = 0
-    cursors = []
+
+    # 좌표들 간격 중 가장 큰 간격
+    max_distance = 0
+    prev_idx = 0
+    max_inf = []
+
+    cursor_cnt = [0]
 
     for idx, w in enumerate(name):
         if w != "A":
+            # 알파뱃 count
             answer += move_from_a(w)
-            cursors.append(idx)
 
-    idx = 0
-    prev_cursor = 0
-    while cursors:
-        # 오른쪽 이동
-        a = move_cursor(len(name), prev_cursor, cursors[idx])
-        # 왼쪽 이동
-        b = move_cursor(len(name), prev_cursor, cursors[idx-1])
+            # 커서 카운트
+            # 간격이 가장 큰 경우 >> 가운데를 비운채로 좌, 우의 거리를 구해서 그 합을 구할것이기 때문에 비워질 부분이 가장 큰 케이스가 최소 거리이다.
+            if idx - prev_idx > max_distance:
+                max_distance = idx - prev_idx
+                max_inf = [prev_idx, idx]
+            prev_idx = idx
 
-        # 왼쪽 이동
-        if a > b:
-            prev_cursor = cursors.pop(idx - 1)
-            answer += b
-            idx -= 1
-        # 오른쪽 이동
-        else:
-            prev_cursor = cursors.pop(idx)
-            answer += a
-    return answer
+            # (역방향) 첫번째 A가 아닌곳을 찾아서 역방향으로 도는 케이스 // 최초 1회만 돔
+            if len(cursor_cnt) == 1:
+                cursor_cnt[0] = len(name) - idx
+                cursor_cnt.append(idx)
+            # (정방향) 마지막 A가 아닌곳을 찾아서 계산 // 마지막 한번만 구하면됨
+            else:
+                cursor_cnt[1] = idx
+
+    if max_inf:
+        cursor_cnt.append(len(name) - max_distance + min(len(name)-max_inf[1], max_inf[0]))
+
+    return answer + min(cursor_cnt)
 
 
 def move_from_a(ch):
     return min(ord(ch) - ord("A"), ord("Z") - ord(ch) + 1)
 
 
+# target 까지 가는 방법 결정
 def move_cursor(name_len, cursor_from, cursor_target):
-    return min(abs(cursor_from - cursor_target), -abs(cursor_from - cursor_target) + name_len)
+    # from - target 사이 거리
+    between = abs(cursor_from - cursor_target)
+    return min(between, name_len - between)
 
-print(solution1("JEROEN"))
-print(solution1("JAN"))
-print(solution1("AAB"))
 
-print(move_from_a("J"))
-print(move_from_a("E"))
-print(move_from_a("R"))
-print(move_from_a("O"))
-print(move_from_a("E"))
-print(move_from_a("N"))
+# case 1
+# 무지성 정방향이 나은 경우 >> [-1]
+# case 2
+# 무지성 역방향이 나은 경우 >> [0]
+# case 3
+# 왔다 갔다하기
+
+
+l = ["A"]*20
+l[1] = "B"
+l[5] = "B"
+l[18] = "B"
+a = ""
+for i in l:
+    a+=i
+
+print(a)
+print(solution1(a)) # 9
+print(solution1("AAAA")) # 9
+print(solution1("BAAA")) # 9
+print(solution1("ABAA")) # 9
+print(solution1("AABA")) # 9
+print(solution1("AAAB")) # 9
