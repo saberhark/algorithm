4bf3dabd6ddf3ba012db5e9e9c5b13c03b6e16a2
saberhark <saberhark6@gmail.com>
2023-11-01 17:46:49 +0900

    [완료] 네트워크

--- 프로그래머스/고득점 키트/08.dfs_bfs/네트워크.py
+++ 프로그래머스/고득점 키트/08.dfs_bfs/네트워크.py
@@ -1,29 +1,34 @@
 def solution(n, computers):
-    answer = 0
     paths = []
+    done = set()
 
-    return answer
+    idx = 0
+    while idx < n:
+        # 처음 도달한 컴퓨터
+        if idx not in done:
+            paths.append(dfs(n, [], idx, computers))
+            done.update(paths[-1])
+        idx += 1
 
-
-def dfs(n, idx, computers):
-    a = computers.pop(idx)
-
-    for i in range(idx+1, n):
-        if a[i] == 1:
-            dfs(n, i, computers[idx ])
-
-    # idx 가 1이면 1+1 부터 탐색하면 됨
-    for i in range(idx+1, n):
-        computers[i]
-
-    return
+    return len(paths)
 
 
+def dfs(n, path, idx, computers):
+    links = computers[idx]
+
+    for i in range(n):
+        
+        # idx+1부터 보면 안됨 역방향으로 갈 수도 있어서. path에 있는지 확인해야됨 안그러면 무한 루프에 빠짐
+        if links[i] == 1 and i not in path:
+            path.append(i)
+            dfs(n, path, i, computers)
+
+    return path
 
 
 com = [[1, 1, 0],
-        [1, 1, 0],
-        [0, 0, 1]]
-
+       [1, 1, 0],
+       [0, 0, 1]]
 
 print(solution(3, com))
+print(solution(4, [[1, 0, 0, 1], [0, 1, 0, 0], [0, 0, 1, 1], [1, 0, 1, 1]]))
