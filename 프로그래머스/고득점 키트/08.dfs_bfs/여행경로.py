8e5948d16dc18cac6cc9990c285f95d70bc517fc
saberhark <saberhark6@gmail.com>
2023-11-07 17:23:45 +0900

    [완료] dfs 여행경로

--- 프로그래머스/고득점 키트/08.dfs_bfs/여행경로.py
+++ 프로그래머스/고득점 키트/08.dfs_bfs/여행경로.py
@@ -3,7 +3,7 @@ def solution(tickets):
     start_to_end = {}
 
     # 딕셔너리로 변환
-    for start, end in sorted(tickets, key=lambda item : item[1], reverse=True):
+    for start, end in sorted(tickets, key=lambda item : item[1]):
         if start_to_end.get(start):
             start_to_end[start].append(end)
         else:
@@ -18,28 +18,35 @@ def solution(tickets):
 # 잘못된 길로 왔을 떄 다시 기어 올라 가야되는데 그러질 못함.
 # 표시한곳 다시 보기
 def dfs(ticket_dict, start, path):
-    print(start, ticket_dict)
     if ticket_dict.get(start):
-        next_ticket = ticket_dict[start].pop()
-        path.append(next_ticket)
+        for _ in range(len(ticket_dict[start])):
+            next_ticket = ticket_dict[start].pop(0)
+            path.append(next_ticket)
 
-        # 다음 행성지가 없는 출발지 제거
-        if not ticket_dict[start]:
-            ticket_dict.pop(start)
+            # 다음 행선지가 없는 출발지 제거
+            if not ticket_dict[start]:
+                ticket_dict.pop(start)
 
-        # 다 탐색 했으면
-        if not ticket_dict:
-            return
+            # 다 탐색 했으면
+            if not ticket_dict:
+                return
 
-        # 다음 탐색 시작
-        dfs(ticket_dict, next_ticket, path)
-    # 잘못된 길로 왔을 떄
+            # 다음 탐색 시작
+            dfs(ticket_dict, next_ticket, path)
+
+            # 다 탐색 했으면
+            if not ticket_dict:
+                return
+
+                # 완상 복구
+            if ticket_dict.get(start):
+                ticket_dict[start].append(next_ticket)
+            else:
+                ticket_dict[start] = [next_ticket]
+        path.pop()
     else:
-        if ticket_dict.get(path[-2]):
-            ticket_dict[path[-2]].append(path.pop())
-        else:
-            ticket_dict[path[-2]] = [path.pop()]
-        # 여기 살펴 봐야됨
+        # 잘못된 길로 왔을 때
+        path.pop()
 
 # print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
 # print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]))
@@ -52,38 +59,38 @@ def a(test_case, answer):
     else:
         print("pass")
 
-# a([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]],
-#   ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"])
-#
-# a([["ICN", "AOO"], ["AOO", "BOO"], ["BOO", "COO"], ["COO", "DOO"], ["DOO", "EOO"], ["EOO", "DOO"], ["DOO", "COO"], ["COO", "BOO"], ["BOO", "AOO"]],
-#   ["ICN", "AOO", "BOO", "COO", "DOO", "EOO", "DOO", "COO", "BOO", "AOO"])
-#
-# a([["ICN", "AOO"], ["AOO", "BOO"], ["AOO", "COO"], ["COO", "AOO"], ["BOO", "ZOO"]],
-#   ["ICN", "AOO", "COO", "AOO", "BOO", "ZOO"])
-#
-# a([["ICN", "AOO"], ["AOO", "BOO"], ["AOO", "BOO"], ["BOO", "AOO"], ["BOO", "FOO"], ["FOO", "COO"], ["COO", "ZOO"]],
-#   ["ICN", "AOO", "BOO", "AOO", "BOO", "FOO", "COO", "ZOO"])
-#
-# a([["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]],
-#   ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"])
-#
-# a([["ICN", "AAA"], ["ICN", "AAA"], ["ICN", "AAA"], ["AAA", "ICN"], ["AAA", "ICN"]], [
-#     "ICN", "AAA", "ICN", "AAA", "ICN", "AAA"])
-#
-# a([["ICN", "BBB"],["ICN", "CCC"],["BBB", "CCC"],["CCC", "BBB"],["CCC", "ICN"]],
-#   ["ICN", "BBB", "CCC", "ICN", "CCC", "BBB"])
-#
-# a([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]],
-#   ["ICN", "JFK", "HND", "IAD"])
-#
-# a([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]],
-#   ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"])
-#
-# a([["ICN", "AOO"], ["ICN", "AOO"], ["AOO", "ICN"], ["AOO", "COO"]],
-#   ["ICN", "AOO", "ICN", "AOO", "COO"])
-#
-# a([["ICN", "AAA"], ["ICN", "AAA"], ["ICN", "AAA"], ["AAA", "ICN"], ["AAA", "ICN"]],
-#   ["ICN", "AAA", "ICN", "AAA", "ICN", "AAA"])
+#a([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]],
+#  ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"])
+
+a([["ICN", "AOO"], ["AOO", "BOO"], ["BOO", "COO"], ["COO", "DOO"], ["DOO", "EOO"], ["EOO", "DOO"], ["DOO", "COO"], ["COO", "BOO"], ["BOO", "AOO"]],
+  ["ICN", "AOO", "BOO", "COO", "DOO", "EOO", "DOO", "COO", "BOO", "AOO"])
+
+a([["ICN", "AOO"], ["AOO", "BOO"], ["AOO", "COO"], ["COO", "AOO"], ["BOO", "ZOO"]],
+  ["ICN", "AOO", "COO", "AOO", "BOO", "ZOO"])
+
+a([["ICN", "AOO"], ["AOO", "BOO"], ["AOO", "BOO"], ["BOO", "AOO"], ["BOO", "FOO"], ["FOO", "COO"], ["COO", "ZOO"]],
+  ["ICN", "AOO", "BOO", "AOO", "BOO", "FOO", "COO", "ZOO"])
+
+a([["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]],
+  ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"])
+
+a([["ICN", "AAA"], ["ICN", "AAA"], ["ICN", "AAA"], ["AAA", "ICN"], ["AAA", "ICN"]], [
+    "ICN", "AAA", "ICN", "AAA", "ICN", "AAA"])
+
+a([["ICN", "BBB"],["ICN", "CCC"],["BBB", "CCC"],["CCC", "BBB"],["CCC", "ICN"]],
+  ["ICN", "BBB", "CCC", "ICN", "CCC", "BBB"])
+
+a([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]],
+  ["ICN", "JFK", "HND", "IAD"])
+
+a([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]],
+  ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"])
+
+a([["ICN", "AOO"], ["ICN", "AOO"], ["AOO", "ICN"], ["AOO", "COO"]],
+  ["ICN", "AOO", "ICN", "AOO", "COO"])
+
+a([["ICN", "AAA"], ["ICN", "AAA"], ["ICN", "AAA"], ["AAA", "ICN"], ["AAA", "ICN"]],
+  ["ICN", "AAA", "ICN", "AAA", "ICN", "AAA"])
 
 a([["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]],
   ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"])
